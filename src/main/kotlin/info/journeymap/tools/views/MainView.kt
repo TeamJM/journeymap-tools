ef557aebac72e542277336d3b0f4cde82245db83
Gareth Coles <gdude2002@gmail.com>
2020-04-25 12:08:37 +0100

    Just working out TornadoFX

--- src/main/kotlin/info/journeymap/tools/views/MainView.kt
+++ src/main/kotlin/info/journeymap/tools/views/MainView.kt
@@ -1,10 +1,31 @@
 package info.journeymap.tools.views
 
+import javafx.beans.property.StringProperty
+import javafx.scene.layout.Priority
 import tornadofx.*
 
 class MainView : View() {
     override val root = vbox {
+        this.paddingAll = 10
+        this.spacing = 10.0
+
+        hbox {
+            textfield("Field") {
+                this.hgrow = Priority.ALWAYS
+            }
+            button("Browse") {
+                this.hgrow = Priority.ALWAYS
+                this.minWidth = 50.0
+            }
+
+            this.spacing = 10.0
+        }
         button("Press me")
         label("Waiting")
+    }
+
+    init {
+        this.setWindowMinSize(300, 100)
+        this.title = "JourneyMap Tools"
     }
 }
