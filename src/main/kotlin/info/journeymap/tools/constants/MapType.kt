d60867a3dd55845c3672bf26ea676db5b299dca1
Mysticdrew <drewhaas@gmail.com>
2022-01-23 14:11:33 -0600

    added biome maptype

--- src/main/kotlin/info/journeymap/tools/constants/MapType.kt
+++ src/main/kotlin/info/journeymap/tools/constants/MapType.kt
@@ -4,6 +4,7 @@ enum class MapType(val type: String) {
     SURFACE_DAY("Surface (Day)"),
     SURFACE_NIGHT("Surface (Night)"),
     SURFACE_TOPO("Surface (Topographic)"),
+    BIOME("Biome"),
     UNDERGROUND("Underground");
 
     override fun toString(): String {
